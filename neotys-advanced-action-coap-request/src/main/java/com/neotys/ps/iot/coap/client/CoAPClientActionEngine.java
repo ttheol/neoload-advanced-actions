f9b7c0be63fbdf4bc9ffafd5763e4f88e136ee42
Thomas Théol <thomas.theol@neotys.com>
2017-10-27 18:01:09 +0200

    Cleaned the action engine

--- neotys-advanced-action-coap-request/src/main/java/com/neotys/ps/iot/coap/client/CoAPClientActionEngine.java
+++ neotys-advanced-action-coap-request/src/main/java/com/neotys/ps/iot/coap/client/CoAPClientActionEngine.java
@@ -191,15 +191,7 @@ public final class CoAPClientActionEngine implements ActionEngine {
 	public void stopExecute() {
 		// TODO add code executed when the test have to stop.
 	}
-	
-	public static byte[] hexStringToByteArray(String s) {
-	    int len = s.length();
-	    byte[] data = new byte[len / 2];
-	    for (int i = 0; i < len; i += 2) {
-	        data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-	                             + Character.digit(s.charAt(i+1), 16));
-	    }
-	    return data;
-	}
+
+
 
 }
